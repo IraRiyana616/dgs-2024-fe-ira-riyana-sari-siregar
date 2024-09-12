50605dc577e76b24e211ea00a2d3d4df9c91007f
Ira Riyana <irariyanasari@gmail.com>
2024-09-12 15:55:46 +0800

    perbaikan

--- src/components/transaction/TransactionCard.jsx
+++ src/components/transaction/TransactionCard.jsx
@@ -1,44 +1,101 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const TransactionCard = ({ transaction, onDelete }) => {
+  const [showConfirm, setShowConfirm] = useState(false);
+
+  const handleDeleteClick = () => {
+    setShowConfirm(true);
+  };
+
+  const handleConfirmDelete = () => {
+    onDelete(transaction.id);
+    setShowConfirm(false);
+  };
+
+  const handleCancelDelete = () => {
+    setShowConfirm(false);
+  };
+
   return (
-    <div className="bg-gray-100 shadow-md rounded-lg p-4">
-      <div className="flex items-center">
-        <div className="flex-shrink-0">
-          {transaction.category === 'Restaurants & Cafe' && (
+    <div>
+      {/* Transaction Card */}
+      <div className="bg-gray-100 shadow-md rounded-lg p-4">
+        <div className="flex items-center">
+          <div className="flex-shrink-0">
+            {transaction.category === 'Restaurants & Cafe' && (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6 text-blue-500"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor">
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M7 20l4-16m2 16l4-16m-6 16H7m6-6v6m4-6v6m-4-6h.01M9 16h.01"
+                />
+              </svg>
+            )}
+            {transaction.category === 'Clothes & Shopping' && (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6 text-purple-500"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor">
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M16 4v12l-4-2-4 2V4M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
+                />
+              </svg>
+            )}
+            {transaction.category === 'Credit & Loans' && (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6 text-yellow-500"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor">
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                />
+              </svg>
+            )}
+            {transaction.category === 'Gifts Card' && (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6 text-red-500"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor">
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+            )}
+          </div>
+          <div className="ml-4">
+            <h2 className="text-lg font-medium">{transaction.category}</h2>
+            <p className="text-gray-500">{transaction.date}</p>
+          </div>
+        </div>
+        <div className="flex justify-end mt-2">
+          <p className="text-gray-600 font-medium">${transaction.amount}</p>
+          <button
+            onClick={handleDeleteClick}
+            className="ml-4 p-2 rounded-md bg-gray-200 text-gray-600 justify-end">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6 text-blue-500"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor">
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M7 20l4-16m2 16l4-16m-6 16H7m6-6v6m4-6v6m-4-6h.01M9 16h.01"
-              />
-            </svg>
-          )}
-          {transaction.category === 'Clothes & Shopping' && (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6 text-purple-500"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor">
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M16 4v12l-4-2-4 2V4M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
-              />
-            </svg>
-          )}
-          {transaction.category === 'Credit & Loans' && (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6 text-yellow-500"
+              className="h-5 w-5"
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor">
@@ -49,48 +106,32 @@ const TransactionCard = ({ transaction, onDelete }) => {
                 d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
               />
             </svg>
-          )}
-          {transaction.category === 'Gifts Card' && (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6 text-red-500"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor">
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-              />
-            </svg>
-          )}
-        </div>
-        <div className="ml-4">
-          <h2 className="text-lg font-medium">{transaction.category}</h2>
-          <p className="text-gray-500">{transaction.date}</p>
+          </button>
         </div>
       </div>
-      <div className="flex justify-end mt-2">
-        <p className="text-gray-600 font-medium">${transaction.amount}</p>
-        <button
-          onClick={() => onDelete(transaction.id)}
-          className="ml-4 p-2 rounded-md bg-gray-200 text-gray-600 justify-end">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-5 w-5"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor">
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-            />
-          </svg>
-        </button>
-      </div>
+
+      {showConfirm && (
+        <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50">
+          <div className="bg-white p-6 rounded-lg shadow-md">
+            <h3 className="text-lg font-semibold">Confirm Delete</h3>
+            <p className="mt-2 text-gray-600">
+              Are you sure you want to delete this transaction?
+            </p>
+            <div className="flex justify-end mt-4">
+              <button
+                onClick={handleCancelDelete}
+                className="px-4 py-2 bg-gray-300 text-gray-700 rounded-md mr-2">
+                Cancel
+              </button>
+              <button
+                onClick={handleConfirmDelete}
+                className="px-4 py-2 bg-red-500 text-white rounded-md">
+                Delete
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
